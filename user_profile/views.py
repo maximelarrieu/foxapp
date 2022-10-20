805a1dd3359c8cb841084df956197a6bee992f22
maximelarrieu <maxime.larrieu@ynov.com>
2022-10-20 14:38:29 +0200

    :sparkles: add profile render function

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@login_required
+def profile_page(request):
+    return render(request, 'user_profile/index.html')
