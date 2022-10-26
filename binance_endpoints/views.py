9e815c3ea4a9b4c1b7b6a044046ef8daec63dce9
maximelarrieu <maxime.larrieu@ynov.com>
2022-10-26 13:53:58 +0200

    :zap: improve calls

--- binance_endpoints/views.py
+++ binance_endpoints/views.py
@@ -24,8 +24,8 @@ def btc_price(request):
     btcusdt_price = get_btc_usdt_price(request)
     user_assets = client.asset_detail()
 
-    mining_account_list = client.mining_account_list("sha256", "450267197")
-    print("mining account", mining_account_list)
+    # mining_account_list = client.mining_account_list("sha256", "450267197")
+    # print("mining account", mining_account_list)
 
     nft = client.nft_transaction_history(0)
 
@@ -36,7 +36,6 @@ def btc_price(request):
     return render(request, 'binance_endpoints/index.html', context={
         'btcusdt_price': btcusdt_price,
         'user_assets': user_assets,
-        'mining_account_list': mining_account_list,
         'nft': nft,
         'stacking': stacking,
         'exchange': exchange
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,9 +1,21 @@
+from binance.spot import Spot
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+def binance_connector():
+    client = Spot(key='OJmlpfKzP5iE0DamvtIt0J5YI5IrP8KRodtTB9aeQypI36oET1DbGixLz0Mkx65k', secret='spgUQE5EUu5dip6tEZKK80p8SFWhGVk6hSdc7gAkGDjVMJhwvLiUpvGG1PB4bwWH')
+
+    return client
+
+
 @login_required
 def profile_page(request):
     btcusdt_price = request.session.get('btcusdt_price')
+
     return render(request, 'user_profile/index.html', context={'btcusdt_price': btcusdt_price})
+
+
+def get_nft_list(request):
+    nft = client.nft
